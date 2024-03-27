f73a7d0e03473d2c782f04bd44ca9859b235c689
Edgar Roman Cervantes <redvant@outlook.com>
2024-03-27 13:11:57 -0600

    Refactor parse templates at startup
    Add Controllers package with StaticHandler

--- main.go
+++ main.go
@@ -2,48 +2,44 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"path/filepath"
 
+	"github.com/redvant/lenslocked/controllers"
 	"github.com/redvant/lenslocked/middleware"
 	"github.com/redvant/lenslocked/views"
 )
 
-func executeTemplate(w http.ResponseWriter, filepath string, data interface{}) {
-	t, err := views.Parse(filepath)
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
-		return
-	}
-	t.Execute(w, data)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "Page not found", http.StatusNotFound)
-		return
-	}
-	tplPath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tplPath, nil)
-}
-
-func contactHandler(w http.ResponseWriter, _ *http.Request) {
-	tplPath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tplPath, nil)
-}
-
-func faqHandler(w http.ResponseWriter, _ *http.Request) {
-	tplPath := filepath.Join("templates", "faq.gohtml")
-	executeTemplate(w, tplPath, nil)
-}
-
 func main() {
 	router := http.NewServeMux()
-	router.HandleFunc("GET /", homeHandler)
-	router.HandleFunc("GET /contact", contactHandler)
-	router.HandleFunc("GET /faq", faqHandler)
+
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+
+	// Add notFound check to StaticHandler for "/"
+	router.HandleFunc("GET /", func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path != "/" {
+				http.NotFound(w, r)
+				return
+			}
+			next.ServeHTTP(w, r)
+		}
+	}(controllers.StaticHandler(tpl)))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	router.HandleFunc("GET /contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	router.HandleFunc("GET /faq", controllers.StaticHandler(tpl))
 
 	server := http.Server{
 		Addr:    ":3000",
