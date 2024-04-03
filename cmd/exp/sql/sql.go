e1e2c66e2197b2d6cd3518c8fec9f18a3d92049d
Edgar Roman Cervantes <redvant@outlook.com>
2024-04-03 08:21:29 -0600

    Add db connection example code

---
+++ cmd/exp/sql/sql.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func main() {
+	db, err := sql.Open("pgx",
+		"host=localhost port=5432 user=baloo password=junglebook dbname=lenslocked sslmode=disable",
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected!")
+}
