b8749258b944c94a13bde8290b4ca94909ae6925
Edgar Roman Cervantes <redvant@outlook.com>
2024-04-04 08:09:41 -0600

    Add insert orders example code

--- cmd/exp/sql/sql.go
+++ cmd/exp/sql/sql.go
@@ -92,4 +92,18 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("User %d - Name:%q Email:%q\n", id, name, email)
+
+	userID := 1
+	for i := 1; i <= 5; i++ {
+		amount := i * 100
+		desc := fmt.Sprintf("Fake order #%d", i)
+		_, err := db.Exec(`
+			INSERT INTO orders(user_id, amount, description)
+			VALUES($1, $2, $3)
+		`, userID, amount, desc)
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println("Created orders.")
 }
