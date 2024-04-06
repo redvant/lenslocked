b0110cc7d6671f8b80fd07de03f306897f840429
Edgar Roman Cervantes <redvant@outlook.com>
2024-04-06 08:58:03 -0600

    Fix password_hash db column name

--- models/users.go
+++ models/users.go
@@ -31,7 +31,7 @@ func (us *UserService) Create(email, password string) (*User, error) {
 		PasswordHash: passwordHash,
 	}
 	row := us.DB.QueryRow(`
-		INSERT INTO users(email, passwordHash)
+		INSERT INTO users(email, password_hash)
 		VALUES($1, $2) RETURNING id;
 	`, email, passwordHash)
 	err = row.Scan(&user.ID)
