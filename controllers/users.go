8a1ad5820dfa7f24d3d6d27e425058e66d273161
Edgar Roman Cervantes <redvant@outlook.com>
2024-04-06 08:59:53 -0600

    Call UserService.Create in users controller

--- controllers/users.go
+++ controllers/users.go
@@ -23,6 +23,13 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, r.FormValue("email"))
-	fmt.Fprintln(w, r.FormValue("password"))
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	user, err := u.UserService.Create(email, password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "User created: %+v", *user)
 }
