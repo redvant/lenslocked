784ea54cee9bbe321e777bd27e33f83e7629e48b
Edgar Roman Cervantes <redvant@outlook.com>
2024-04-13 09:54:36 -0600

    Add sessions user_id FK

--- models/sql/sessions.sql
+++ models/sql/sessions.sql
@@ -1,5 +1,9 @@
 CREATE TABLE sessions (
     id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE,
+    user_id INT UNIQUE REFERENCES users (id),
     token_hash TEXT UNIQUE NOT NULL
 );
+
+ALTER TABLE sessions
+ADD CONSTRAINT sessions_user_id_fkey
+FOREIGN KEY (user_id) REFERENCES users (id);
