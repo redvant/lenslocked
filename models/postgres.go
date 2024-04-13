d74497a9054f2b64ecebe778d805d9707704b477
Edgar Roman Cervantes <redvant@outlook.com>
2024-04-13 17:40:18 -0600

    Add goose Migrate on startup

--- models/postgres.go
+++ models/postgres.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
 )
 
 // Open will open a SQL connection with the provided
@@ -43,4 +44,16 @@ func (cfg PostgresConfig) String() string {
 	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode,
 	)
+}
+
+func Migrate(db *sql.DB, dir string) error {
+	err := goose.SetDialect("postgres")
+	if err != nil {
+		return fmt.Errorf("migrage: %w", err)
+	}
+	err = goose.Up(db, dir)
+	if err != nil {
+		return fmt.Errorf("migrage: %w", err)
+	}
+	return nil
 }
