58a45b08884b5331cd9371c4a4f1c627b482dfff
Edgar Roman Cervantes <redvant@outlook.com>
2024-04-23 22:34:19 -0600

    Add UserService.UpdatePassword

--- controllers/users.go
+++ controllers/users.go
@@ -161,7 +161,12 @@ func (u Users) ProcessResetPassword(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Something went wrong", http.StatusInternalServerError)
 		return
 	}
-	// TODO: Update user's password.
+	err = u.UserService.UpdatePassword(user.ID, data.Password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
 
 	// Sign the user in now that their password has been reset.
 	// Any errors from this point should redirect the user to the sign in page.
