5fb707641635b61c2c1e69d0bab1de7ec762f000
Edgar Roman Cervantes <redvant@outlook.com>
2024-05-11 21:06:21 -0600

    Implement file content and extension checks

--- models/galleries.go
+++ models/galleries.go
@@ -176,9 +176,17 @@ func (gs *GalleryService) Image(galleryID int, filename string) (Image, error) {
 	}, nil
 }
 
-func (gs *GalleryService) CreateImage(galleryID int, filename string, contents io.Reader) error {
+func (gs *GalleryService) CreateImage(galleryID int, filename string, contents io.ReadSeeker) error {
+	err := checkContentType(contents, gs.imageContentTypes())
+	if err != nil {
+		return fmt.Errorf("creating image %v: %w", filename, err)
+	}
+	err = checkExtension(filename, gs.extensions())
+	if err != nil {
+		return fmt.Errorf("creating image %v: %w", filename, err)
+	}
 	galleryDir := gs.galleryDir(galleryID)
-	err := os.MkdirAll(galleryDir, 0o755)
+	err = os.MkdirAll(galleryDir, 0o755)
 	if err != nil {
 		return fmt.Errorf("creating gallery-%d images directory: %w", galleryID, err)
 	}
@@ -209,6 +217,10 @@ func (gs *GalleryService) DeleteImage(galleryID int, filename string) error {
 
 func (gs *GalleryService) extensions() []string {
 	return []string{".png", ".jpg", ".jpeg", ".gif"}
+}
+
+func (gs *GalleryService) imageContentTypes() []string {
+	return []string{"image/png", "image/jpeg", "image/gif"}
 }
 
 func (gs *GalleryService) galleryDir(id int) string {
